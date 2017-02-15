4a541dd60d3bccc29f7a24dbb5818adc8215d3b9
4thirteen2one <minc123.f5@outlook.com>
2017-02-16 01:53:25 +0800

    almost done,except lacking for classifying

--- get_schedule.py
+++ get_schedule.py
@@ -1,47 +1,46 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
-import os,sys,re
+# Name:爬取教务系统学生课表
+# Version:0.1
+# Author:4thirteen2one
+
+import os
+import sys
+import re
 import requests
 from bs4 import BeautifulSoup
 import getpass
 # from prettytable import PrettyTable
 
-def get_sessionID(url):
-    global s
-    r = s.get(url)
-    print('Get session: {}'.format(r.status_code))
-    sessionID = r.headers['Set-Cookie'].split(';')[0].split('=')[1]
-    return sessionID
+s = requests.session()
+    # 请求头
+headers = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0"}
 
-def sheet_req(url):
-    '''
-    <input type="hidden" 
-           name="__VIEWSTATE" 
-           id="__VIEWSTATE" 
-           value="/wEPDwUKMTQ4NjM5NDA3OWQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFCGJ0bkxvZ2luU077LK9itKNe3fhI7aoZZ+S5Ryo=" />
-    <input type="hidden" 
-           name="__EVENTVALIDATION" 
-           id="__EVENTVALIDATION" 
-           value="/wEWBQKOmrqLAwKl1bKzCQKC3IeGDAK1qbSRCwLO44u1DVzfq830wXTY29pyqB1kTMdgWLfG" />
-    '''
-    global s
+def sel_port():
+    port = input('请选择查询端口（81端口/84端口）：')
+    host = 'http://jwc.ctgu.edu.cn:{}/'.format(port)
+    return host
+
+def user_info():
+    name = input('请输入您的学号：')
+    password = getpass.getpass('请输入您的密码：')
+    print('\n')
+    return name,password
+
+def aspx_id(url):
+    "获取提交表单中要求的__VIEWSTATE和__EVENTVALIDATION"
     r = s.get(url)
-    print('Get sheet_req:{}'.format(r.status_code))
-    bs = BeautifulSoup(r.text,'lxml')
+    bs = BeautifulSoup(r.text.encode('utf-8'),'lxml')
     viewstate = bs.find('input',{'id':'__VIEWSTATE'})['value']
     eventvalidation = bs.find('input',{'id':'__EVENTVALIDATION'})['value']
-    return (viewstate,eventvalidation)
+    print('Update ASPX ID:{}'.format(r.status_code))
+    return viewstate,eventvalidation
 
-def get_captcha(url):
-    '''
-    <img title="不区分大小写!红色数字,黑色字母!" 
-         id="ImageCheck" 
-         style="border-width:0px;" 
-         src="ValidateCode.aspx">
-    '''
+def get_captcha():
+    "获取验证码，在本地打开并手动输入看到的验证码"
+    captcha_url = 'jwc_glxt/ValidateCode.aspx'
+    r = s.get(host+captcha_url,stream=True)
     # 验证码图片尺寸：60 * 25
-    global s
-    r = s.get(url,stream=True)
     print('Get captcha: {}'.format(r.status_code))
     captcha = r.content
     # 保存验证码至当前文件夹
@@ -54,51 +53,49 @@ def get_captcha(url):
         jpg.close
     os.startfile('captcha.jpg')
     # 这里调用世界上最精密最先进的OCR设备——人的眼睛
-    checkcode = input('Enter the captcha：')
+    checkcode = input('Enter captcha：')
     return checkcode
 
-def log_in(url):
-    global s
-    login_info = {'__VIEWSTATE': viewstate,
-                  '__EVENTVALIDATION': eventvalidation,
-                  'txtUserName': str(name),
-                  'txtPassword':str(password),
-                  'btnLogin.x':"57", 'btnLogin.y':"23",
-                  'CheckCode':checkcode}
+def log_in():
+    "提交用户名、密码和验证码，以及通过获取得到的表单其他项，登录教务系统"
+    login_url = 'jwc_glxt/Login.aspx'
+    # 登陆按钮图片尺寸：69*43
+    viewstate,eventvalidation = aspx_id(host+login_url)
+    login_info = {'__VIEWSTATE':viewstate,
+                  '__EVENTVALIDATION':eventvalidation,
+                  'txtUserName':name,
+                  'txtPassword':password,
+                  'CheckCode': checkcode,
+                  'btnLogin.x':"57",
+                  'btnLogin.y':"23",}
     r = s.post(host+login_url,data=login_info)
     print('Log in: {}'.format(r.status_code))
 
-def get_schedule(url):
-    '''
-    <table class="GridViewStyle" 
-           rules="all" 
-           id="ctl00_MainContentPlaceHolder_GridScore" 
-           style="border-collapse:collapse;" 
-           cellspacing="0" border="1">
-    '''
-    global s
-    init = s.get(url)
-    bs = BeautifulSoup(init.content.decode('utf-8'),'lxml')
-    table0 = bs.find('table',{'id':'ctl00_MainContentPlaceHolder_GridScore'})
-    schedule = []
-    for child in table0.children:
-        box = []
-        for i in child:
-            diff = []
-            if hasattr(i,'get_text'):
-                pattern = r'[\u4e00-\u9fa5]+ [A-Z]-\d{3,4}\s\d\d-\d\d?[\u4e00-\u9fa5]\s[\u4e00-\u9fa5]+''
-                if re.match(pattern,i.get_text()):
-                    pass
-                box.append(i.get_text().replace(u'\xa0',u' '))
-        schedule.append(box)
-    with open('table.txt','w+',encoding='utf-8') as saveit:
-        for box in schedule:
-            for i in box:
-                saveit.write(i)
+def sel_schedule():
+    "选择想要查询的课表,返回网页代码"
+    schedule_url = 'jwc_glxt/Course_Choice/Course_Schedule.aspx'
+    viewstate,eventvalidation = aspx_id(host+schedule_url)
 
-    '''
-    sel_year = int(input('请输入需要查询的学年：'))
-    sel_term = int(input('请输入需要查询的学期：'))
+    status1 = False
+    status2 = False
+    counter = 0
+    separating = '-' * 32 + '\n'
+    while not(status1 and status2):
+        print(separating)
+        sel_year = input('请输入需要查询的学年：')
+        sel_term = input('请输入需要查询的学期：')
+        print(separating)
+        status1 = 1996 <= int(sel_year) <= 2023
+        status2 = 1 <= int(sel_term) <= 4
+        if status1 and status2:
+            print("输入有效！\n")
+            break
+        else:
+            print("输入非法！请重新输入！")
+        counter += 1
+        if counter >= 3:
+            print("你4不4傻？")
+
     select = {'__EVENTTARGET':'',
               '__EVENTARGUMENT':'',
               '__VIEWSTATE':viewstate,
@@ -107,51 +104,84 @@ def get_schedule(url):
               'ctl00$MainContentPlaceHolder$School_Term':sel_term,
               'ctl00$MainContentPlaceHolder$BtnSearch.x':'18',
               'ctl00$MainContentPlaceHolder$BtnSearch.y':'3'}
-    
-    r = s.post(url,data=select)
-    print('Get schedule:{}'.format(r.status_code))
-    bs = BeautifulSoup(r.text,'lxml')
-    table1 = bs.find('table',{'id':'ctl00_MainContentPlaceHolder_GridScore'})
-    for child in table1.children:
-        print(child)
-    print(len(table1))'''
+    r = s.post(host+schedule_url,data=select)
+    print('Select schedule:{}\n'.format(r.status_code))
+    return r
 
-def log_out(url):
-    global s
-    xttc = s.get(url)
+def get_schedule(page):
+    "从当前页面获取课表,并将当前课表网页源码保存至本地"
+    bs = BeautifulSoup(page.text.encode('utf-8'),'lxml')
+    # bs4.BeautifulSoup
+    table = bs.find('table',{'id':'ctl00_MainContentPlaceHolder_GridScore'})
+    # bs4.element.Tag
+    with open('table0.html','w+',encoding='utf-8') as f:
+        f.write(str(table))
+    try:
+        print(table)
+    except UnicodeEncodeError:
+        print('显示不了！\n去特喵的巨硬!\n你，赶紧给劳资换MAC！\n')
+    finally:
+        print('你还不换用python3.6！\n')
+    return table
+
+def listify(table):
+    "接受一段bs4.element.Tag对象的html表格代码，返回一个列表"
+    schedule = []
+    for tr in table.find_all('tr'):
+        box = []
+        for th in tr.find_all('th'):
+            box.append(th.get_text())
+        for td in tr.find_all('td'):
+            box.append(td.get_text().replace(u'\xa0',u' '))
+        schedule.append(box)
+    return schedule
+
+def query_course(schedule):
+    "输入课程节次和日数，查询课程"
+    print('查询课程：')
+    quit0not = None
+    status1 = False
+    status2 = False
+    separating = '-' * 32 + '\n'
+    while not(status1 and status2) or (quit0not != 'y'):
+        print(separating)
+        day = int(input('周几啊？'))
+        period = int(input('第几节（以大课为以一单位）：'))
+        print(separating)
+        status1 = 1 <= int(period) <= 6
+        status2 = 1 <= int(day) <= 7
+        if not(status1 and status2):
+            print("输入非法！请重新输入！\n")
+            continue
+        course = schedule[period][day]
+        print(course+'\n')
+        quit0not = input('查询完毕？\n')
+
+def log_out():
+    "登出教务系统"
+    global host,s
+    logout_url = 'jwc_glxt/Login.aspx?xttc=1'
+    xttc = s.get(host+logout_url)
     print('Log out: {}'.format(xttc.status_code))
+    return xttc.status_code
 
 if __name__ == '__main__':
-    print('欢迎使用闵冲的自制查询课表脚本！')
-    port = input('请选择查询端口：81端口 or 84端口？')
-    name = input('请输入您的学号：')
-    password = getpass.getpass('请输入您的密码：')
-
-    host = 'http://jwc.ctgu.edu.cn:{}/'.format(port)
-    login_url = 'jwc_glxt/Login.aspx'
-    logout_url = 'jwc_glxt/Login.aspx?xttc=1'
-    captcha_url = 'jwc_glxt/ValidateCode.aspx'
-    schedule_url = 'jwc_glxt/Course_Choice/Course_Schedule.aspx'
-    s = requests.session()
-    
-    sessionID = get_sessionID(host+login_url)
-    checkcode = get_captcha(host+captcha_url)
-    viewstate,eventvalidation = sheet_req(host+login_url)
-
-    headers = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0"}
-    cookies = {'ASP.NET_SessionId':get_sessionID,
-               '_gscu_891807511':''}
-
-    # 登陆按钮图片尺寸：69*43
-    log_in(host+login_url)
-    get_schedule(host+schedule_url)
-    log_out(host+logout_url)
-    '''
-    选修课表：http://jwc.ctgu.edu.cn:81/jwc_glxt/Course_Choice/Course_Schedule.aspx
-    id="ct100_Menu1n9"
-    主课表：id="ctl00_MainContentPlaceHolder_GridScore"
-    作业设计：id="ctl00_MainContentPlaceHolder_Label1"
-    选择学年：id="ctl00_MainContentPlaceHolder_School_Year"
-    选择学期：id="ctl00_MainContentPlaceHolder_School_Term"
-    搜索：id="ctl00_MainContentPlaceHolder_BtnSearch"
-    '''
+    print('欢迎使用4thirteen2one的自制查询课表脚本！\n')
+    # 选择端口
+    host = sel_port()
+    # 用户和密码
+    name,password = user_info()
+    # 验证码
+    checkcode = get_captcha()
+    # 登录
+    log_in()
+    # 选择课表查询
+    sche_page = sel_schedule()
+    # 从页面中提取课表
+    table = get_schedule(sche_page)
+    # 列表化课表
+    schedule = listify(table)
+    # 查询课程
+    query_course(schedule)
+    # 登出
+    log_out()
